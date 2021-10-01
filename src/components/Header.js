df60c3f98d2c057456c1cc68e1ad8efbc20e1bc9
hafizaatifkamal <atif.k@valuebound.com>
2021-10-01 20:46:19 +0530

    Adding Cart div

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+
+	const cartItemCount = useSelector((state) => state.allBots.cartItemCount);
+
 	return (
 		<div>
-			<Link to="/">
-				<h3>Dashboard</h3>
-			</Link>
+			<div>
+				<Link to="/">
+					<h3>Dashboard</h3>
+				</Link>
+				<div>
+					<p>Cart - {cartItemCount.length}</p>
+				</div>
+			</div>
 		</div>
 	)
 };
